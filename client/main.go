ff659af6bcd7bf4737e33038534d5b89ff9b26d7
MartinSintas <msintasdesousabispo@frba.utn.edu.ar>
2024-08-09 14:53:42 +0000

    tp0 en Go

--- client/main.go
+++ client/main.go
@@ -3,24 +3,31 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
 
 	// loggear "Hola soy un log" usando la biblioteca log
+	log.Println("Soy un Log") // esto está genial :D pero no queremos (quieren que esté hardcodeado sino que me piden que se pueda leer de un archivo de config)
+
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
 	// validar que la config este cargada correctamente
 
+	if globals.ClientConfig == nil {
+		log.Fatalf("No se pudo cargar la configuracion")
+	}
 	// loggeamos el valor de la config
-
+	log.Println(globals.ClientConfig.Mensaje)
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// enviar un mensaje al servidor con el valor de la config
-
+	utils.EnviarMensaje(globals.ClientConfig.Ip, globals.ClientConfig.Puerto, globals.ClientConfig.Mensaje)
 	// leer de la consola el mensaje
-	// utils.LeerConsola()
+	utils.LeerConsola()
 
 	// generamos un paquete y lo enviamos al servidor
-	// utils.GenerarYEnviarPaquete()
+	utils.GenerarYEnviarPaquete()
+
 }
